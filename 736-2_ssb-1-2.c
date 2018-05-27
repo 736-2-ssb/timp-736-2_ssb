f08137418c2b09a6d680fe7d8f61e9dc92cba070
Шаркова <736-2_ssb@timp2018.novalocal>
2018-05-27 11:52:41 +0000

    commit1-2

---
+++ 736-2_ssb-1-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+   int a,b,c;;
+   printf("Введите числа a и b:\n");
+   scanf("%d",&a);
+   scanf("%d",&b);
+   c=a+b;
+   printf("Сумма=%d\n",c);
+   return 0;
+}
